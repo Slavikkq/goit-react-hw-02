845598290e7098bb478dc0c4e4e73762696eb848
Slavikkq <yaroslavmanuljak@gmail.com>
2024-03-26 22:01:51 +0200

    react hw 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from "react";
 import Feedback from "./components/Feedback/Feedback";
-import Options from "./components/Option/Option";
+import Options from "./components/Options/Options";
 import Notification from "./components/Notification/Notification";
 import Description from "./components/Description/Description";
 
-export default function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+const initialState = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
 
-  useEffect(() => {
+export default function App() {
+  const [feedback, setFeedback] = useState(() => {
     const savedFeedback = JSON.parse(localStorage.getItem("feedback"));
-    if (savedFeedback) {
-      setFeedback(savedFeedback);
-    }
-  }, []);
+    return savedFeedback ?? initialState;
+  });
 
   useEffect(() => {
     localStorage.setItem("feedback", JSON.stringify(feedback));
@@ -24,7 +22,7 @@ export default function App() {
 
   const updateFeedback = (feedbackType) => {
     if (feedbackType === "reset") {
-      setFeedback({ good: 0, neutral: 0, bad: 0 });
+      setFeedback(initialState);
     } else {
       setFeedback((prevFeedback) => ({
         ...prevFeedback,
